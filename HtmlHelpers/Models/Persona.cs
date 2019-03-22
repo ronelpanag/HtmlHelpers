953727ef3d585f27be81e0b2a128b527e98e98b2
Ronel Pantaleón <ronelpanag08@hotmail.com>
2019-03-21 23:59:24 -0400

    Modelo Persona agregado.

---
+++ HtmlHelpers/Models/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HtmlHelpers.Models
+{
+    public class Persona
+    {
+        string Cedula { get; set; }
+        string Nombre { get; set; }
+        string Apellido { get; set; }
+        int Edad { get; set; }
+        string Telefono { get; set; }
+        string CorreoElectronico { get; set; }
+        string Genero { get; set; }
+        string EstadoCivil { get; set; }
+        string Hobby { get; set; }
+    }
+}
